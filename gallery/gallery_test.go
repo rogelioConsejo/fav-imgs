0e080da94e881f4f379c9f937a27deecfc1a731d
rogelioConsejo <rogelio.consejo@gmail.com>
2022-02-27 21:36:04 -0600

    feat: test corrected

--- gallery/gallery_test.go
+++ gallery/gallery_test.go
@@ -17,5 +17,8 @@ func (s stubPersistence) GetImages() []interfaces.Image {
 func TestGalleryInterface(t *testing.T) {
 	var fakePersistence = new(stubPersistence)
 	var testGallery interfaces.Gallery = GetGallery(fakePersistence)
-	fmt.Printf("%+v", testGallery.ImageList())
+	fmt.Printf("%+v\n", testGallery.ImageList())
+	fmt.Printf("%+v\n", testGallery.ImageList()[0])
+	fmt.Printf("%+v\n", testGallery.ImageList()[0].GetTitle())
+	fmt.Printf("%+v\n", testGallery.ImageList()[0].GetUrl())
 }
