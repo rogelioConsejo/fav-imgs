8270e5c6506cffc03af2c798c9d4b6acfd898f74
rogelioConsejo <rogelio.consejo@gmail.com>
2022-02-27 22:08:25 -0600

    feat: basic image to HTML formatting

--- main.go
+++ main.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	. "fav-imgs/gallery"
+	. "fav-imgs/gallery/interfaces"
 	"fmt"
 )
 
 func main() {
-	imageGallery := GetGallery(GetPersistence())
-	fmt.Printf("%+v", imageGallery.ImageList())
+	gallery := GetGallery(GetPersistence())
+	output := formatGallery(gallery)
+	fmt.Println(output)
+}
+
+func formatGallery(gallery Gallery) string {
+	images := gallery.ImageList()
+	formattedImages := ""
+	for _, image := range images {
+		formattedImages += addFormattedImage(image)
+	}
+	return formattedImages
+}
+
+func addFormattedImage(image Image) string {
+	title := image.GetTitle()
+	url := image.GetUrl()
+	return "<div>" + addTitle(title) + addImage(url) + "</div>"
+}
+
+func addTitle(title string) string {
+	return "<p>" + title + "</p>"
+}
+
+func addImage(url string) string {
+	return "<img src=\"" + url + "\">"
 }
